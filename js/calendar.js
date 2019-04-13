f6fa0f587e0fd55399fad81db6442bd302a1be59
zymseo <1047832475@qq.com>
2019-04-13 23:13:16 +0800

    chore: adjust styles

--- js/calendar.js
+++ js/calendar.js
@@ -1,8 +1,8 @@
 /**
- * Description: 日历控件
- * User: zhaoyiming
- * Date: 2017/9/5
- * License: MIT, https://github.com/zymfe/calendar
+ * Description: 日历控件CSS
+ * @author: zhaoyiming
+ * @since: 2017/9/5
+ × License: MIT, https://github.com/zymfe/calendar
  */
 
 ;(function (global, oDoc, factory) {
